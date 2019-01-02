b1ec25cab1b8625ab4553e5f4856baedbfac6250
조영광 <choyoungkwang@nhn.com>
2019-01-02 18:35:30 +0900

    python

--- python/hello.py
+++ python/hello.py
@@ -1,1 +1,15 @@
-print("Hello, Python!");
+# print
+print("Hello, Python!")
+# if
+x = 2
+if ( x < 10 ):
+    print("x is smaller")
+else:
+    print("x is bigger")
+print("finish")
+# while
+x = 10
+while x > 0:
+    print(x)
+    x = x-1
+print("while over")
