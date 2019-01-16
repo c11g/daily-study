b133e47a06605a5c67f252890ed0563171934230
조영광 <choyoungkwang@nhn.com>
2019-01-16 17:55:13 +0900

    py ch13

--- python/bs_ex2.py
+++ python/bs_ex2.py
@@ -7,12 +7,6 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import ssl
-
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
 if len(url) < 1: url = 'http://py4e-data.dr-chuck.net/known_by_Lilya.html'
@@ -21,7 +15,7 @@ count = int(input('Enter count: '))
 position = int(input('Enter position: '))
 
 while count > 0:
-    html = urlopen(url, context=ctx).read()
+    html = urlopen(url).read()
     soup = BeautifulSoup(html, "html.parser")
 
     # Retrieve all of the anchor tags
