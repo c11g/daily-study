ec0e042c1435ea2aaf18263f44864c055b1f0654
조영광 <choyoungkwang@nhn.com>
2019-03-04 10:39:03 +0900

    A ch9

--- algorithm/ch9_sort2.py
+++ algorithm/ch9_sort2.py
@@ -7,14 +7,20 @@ def insertion_sort(a):
 
     for i in range(1, n):
         key = a[i]
-        j = i-1
+        before_index = i-1
 
-        while j >= 0 and key < a[j]:
-            a[j+1] = a[j]
-            j = j-1
+        while a[before_index] > key: # key가 자기보다 좌측에 위치한 원소의 숫자보다 더 작으면
+            
+            a[before_index+1] = a[before_index] # key가 들어갈 자리 확보를 위해 원소들을 모두 한 칸씩 우측으로 옮김
+            
+            before_index = before_index-1 # 0번째 원소에 도달하면 종료하기 위한 조건
+            if (before_index < 0):
+                print('while break!', a)
+                break
+            
             print('while', a)
         
-        a[j+1] = key
+        a[before_index+1] = key
         print('for', a)
 
 a = [2,4,5,1,3]
