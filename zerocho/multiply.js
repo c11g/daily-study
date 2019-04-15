e47ae4cd3535548df5e788709f0d866512992abc
c11g <renimo@hanmail.net>
2019-04-15 15:41:31 +0900

    구구단 개선중

--- zerocho/multiply.js
+++ zerocho/multiply.js
@@ -1,41 +1,60 @@
+const questionDiv = document.querySelector('.question'),
+    time = document.querySelector('.time'),
+    form = document.querySelector('.form'),
+    input = document.querySelector('.input'),
+    startButton = document.querySelector('.start-button'),
+    enterButton = document.querySelector('.enter-button'),
+    historyOl = document.querySelector('.history-list'),
+    scoreDiv = document.querySelector('.score');
+
 function random() {
-    return Math.ceil(Math.random()*9);
+    return Math.ceil(Math.random()*7 + 2);
 }
 
-const body = document.body;
-
-let questionDiv = document.createElement('div'),
-    form = document.createElement('form'),
-    input = document.createElement('input'),
-    button = document.createElement('button'),
-    resultDiv = document.createElement('div'),
-    num1 = random(),
-    num2 = random();
+let num1 = random(),
+    num2 = random(),
+    timer = 10.0,
+    score = 0,
+    isOver = false;
 
 questionDiv.textContent = `${num1} x ${num2} = `;
-body.append(questionDiv);
+startButton.focus();
 
-body.append(form);
-
-input.type = `number`;
-form.append(input);
-input.focus();
-
-button.textContent = `입력`;
-form.append(button);
-
-body.append(resultDiv);
-
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
+    if ( isOver ) return;
+
+    let classname;
+    
     if ( Number(input.value) === num1 * num2 ) {
-        resultDiv.textContent = `정답!`;
         num1 = random();
         num2 = random();
         questionDiv.textContent = `${num1} x ${num2} = `;
+        score = score + 100;
+        classname = 'correct';
     } else {
-        resultDiv.textContent = `땡!`;
+        score = score - 100;
+        classname = 'incorrect';
     }
+    scoreDiv.textContent = score;
+    historyOl.innerHTML += `<li class="${classname}">${num1} x ${num2} = ${input.value}</li>`;
     input.value = '';
     input.focus();
 });
+
+startButton.addEventListener('click', () => {
+    time.textContent = timer;
+    setTimeout(() => {
+        isOver = true;
+        clearInterval(timerId);
+        time.textContent = 'GAME OVER';
+    }, timer*1000);
+
+    const timerId = setInterval(() => {
+        timer = parseFloat(timer - 0.1).toFixed(1);
+        time.textContent = timer;
+    }, 100);
+
+    input.value = '';
+    input.focus();
+})
